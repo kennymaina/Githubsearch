6f2055936796d1585813f4daaabc112afb266277
kennymaina <victormurigi@outlook.com>
2019-03-30 13:39:53 +0300

    initial changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:"https://api.github.com/users/kennymaina?access_token =b7e19011dae2fe6736b3454bb6f56637774ae721"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  apiUrl:"https://api.github.com/users/kennymaina?access_token =b7e19011dae2fe6736b3454bb6f56637774ae721"
 };
 
 /*
