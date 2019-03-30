fc244de99cb96926a479e9cf04dd4579a948455e
kennymaina <victormurigi@outlook.com>
2019-03-30 13:52:34 +0300

    imports in service.ts

--- src/app/gitsearch.service.ts
+++ src/app/gitsearch.service.ts
@@ -1,7 +1,7 @@
-import { map } from 'rxjs/operators';
-import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { environment } from '../environments/environment';
+// import { map } from 'rxjs/operators';
+// import { Injectable } from '@angular/core';
+// import { HttpClient} from '@angular/common/http';
+// import { environment } from '../environments/environment';
 
 
 @Injectable({
