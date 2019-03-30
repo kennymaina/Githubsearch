b846787e38e16891b5449c9494d1bd4329e4121f
kennymaina <victormurigi@outlook.com>
2019-03-30 13:54:31 +0300

    repo url added with a token

--- src/app/gitsearch.service.ts
+++ src/app/gitsearch.service.ts
@@ -20,15 +20,15 @@ export class GitsearchService {
     this.repo = '';
   }
 
-  // getUser(): any {
-  //   return this.http.get("https://api.github.com/users/" + this.username + "?access_token=b7e19011dae2fe6736b3454bb6f56637774ae721")
-  //   .pipe(map(res =>res));
-  // }
-
-  getRepos(): any {
-    return this.http.get("https://api.github.com/users/" + this.username + "/repos?access_token=b7e19011dae2fe6736b3454bb6f56637774ae721")
-    .pipe(map(res => res));
+  getUser(): any {
+    return this.http.get("https://api.github.com/users/" + this.username + "?access_token=b7e19011dae2fe6736b3454bb6f56637774ae721")
+    .pipe(map(res =>res));
   }
+
+  // getRepos(): any {
+  //   return this.http.get("https://api.github.com/users/" + this.username + "/repos?access_token=b7e19011dae2fe6736b3454bb6f56637774ae721")
+  //   .pipe(map(res => res));
+  // }
   updateUser(username:string){
     this.username = username;
   }
